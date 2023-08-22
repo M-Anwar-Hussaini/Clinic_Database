ca820acff97185a5207967da3e87b3a86d966a77
Anwar <anwarhussaini160@gmail.com>
2023-08-22 20:09:52 +0430

    Create the treatment table with its associated table that creates a many-to-many relationshi

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,19 @@ CREATE TABLE medical_histories (
   patient_id INTEGER REFERENCES patients(id),
   status VARCHAR(50)
 );
+
+-- treatment table
+CREATE TABLE treatments(
+  id BIGSERIAL PRIMARY KEY,
+  type VARCHAR(50),
+  name VARCHAR(50)
+);
+
+-- visit table is an associate table that creates the many-to-many relationship
+-- between medical_histories table and treatments table
+CREATE TABLE visits (
+  id BIGSERIAL PRIMARY KEY,
+  visit_date DATE,
+  medical_history_id INTEGER REFERENCES medical_histories(id),
+  treatment_id INTEGER REFERENCES treatments(id)
+);
