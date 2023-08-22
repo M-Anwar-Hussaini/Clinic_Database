77d698c9bb4cc0443ff433cb7f6a01ec7f6d5adf
Anwar <anwarhussaini160@gmail.com>
2023-08-22 20:01:04 +0430

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,10 @@ CREATE TABLE patients (
   date_of_birth DATE
 );
 
+-- medical_histories table
+CREATE TABLE medical_histories (
+  id BIGSERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INTEGER REFERENCES patients(id),
+  status VARCHAR(50)
+);
